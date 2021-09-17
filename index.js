706c6b429e699cdbdaccdf4637adbfc69401819e
IlyaShumyantsev <ilya.shumyantsev@gmail.comm>
2021-09-17 18:52:59 +0300

    add cart and user

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const path = require('path');
 const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const app = express();
 const hbs = exphbs.create({
@@ -16,6 +17,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('6144abecb9471643d6e67294');
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -28,10 +39,19 @@ app.use('/card', cardRoutes);
 async function start() {
   const PORT = process.env.PORT || 3000;
   const password = 'rOXS5EiLVnXxEGUC';
-  const url = `mongodb+srv://ilya:${password}@cloud.gjaim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  const url = `mongodb+srv://ilya:${password}@cloud.gjaim.mongodb.net/shop`;
 
   try {
     await mongoose.connect(url, { useNewUrlParser: true });
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: 'ilya@mail.ru',
+        name: 'Ilya',
+        cart: { items: [] },
+      });
+      await user.save();
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
