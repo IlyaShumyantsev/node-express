b6cce3e9946d2a795a3038590d18e868680f1841
assetbookilya <ilya.shumyantsev@assetbook.com>
2021-09-20 18:19:04 +0300

    add orders

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const ordersRoutes = require('./routes/orders');
 const path = require('path');
 const mongoose = require('mongoose');
 const User = require('./models/user');
@@ -35,6 +36,7 @@ app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
 
 async function start() {
   const PORT = process.env.PORT || 3000;
